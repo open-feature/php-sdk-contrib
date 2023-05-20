0ffe719abfd14595cd5b55875bbe5b29599548d8
Tom Carrio <tom@carrio.dev>
2023-05-20 09:28:46 -0400

    style: import dd-trace hook before use in _autoload.php
    Signed-off-by: Tom Carrio <tom@carrio.dev>

--- hooks/DDTrace/src/_autoload.php
+++ hooks/DDTrace/src/_autoload.php
@@ -2,5 +2,7 @@
 
 declare(strict_types=1);
 
+use OpenFeature\Hooks\DDTrace\DDTraceHook;
+
 // automatically registers the DDTraceHook for OpenFeature
-OpenFeature\Hooks\DDTrace\DDTraceHook::register();
+DDTraceHook::register();
