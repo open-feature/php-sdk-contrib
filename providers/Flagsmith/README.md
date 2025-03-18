4098a0b4c780fedc31195aee012e4c8b68208bc7
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2025-03-18 21:49:04 +0000

    Initial Flagsmith provider.
    Signed-off-by: Chris Lightfoot-Wild <github-clw@wild.me.uk>

---
+++ providers/Flagsmith/README.md
@@ -0,0 +1,47 @@
+# OpenFeature Flagsmith Provider for PHP
+
+[![a](https://img.shields.io/badge/slack-%40cncf%2Fopenfeature-brightgreen?style=flat&logo=slack)](https://cloud-native.slack.com/archives/C0344AANLA1)
+[![Latest Stable Version](http://poser.pugx.org/open-feature/flagsmith-provider/v)](https://packagist.org/packages/open-feature/flagsmith-provider)
+[![Total Downloads](http://poser.pugx.org/open-feature/flagsmith-provider/downloads)](https://packagist.org/packages/open-feature/flagsmith-provider)
+![PHP 8.0+](https://img.shields.io/badge/php->=8.0-blue.svg)
+[![License](http://poser.pugx.org/open-feature/flagsmith-provider/license)](https://packagist.org/packages/open-feature/flagsmith-provider)
+
+## Overview
+
+Flagsmith provides an all-in-one platform for developing, implementing, and managing your feature flags. This repository and package provides the client side code for interacting with it via the OpenFeature PHP SDK.
+
+This package also builds on various PSRs (PHP Standards Recommendations) such as the Logger interfaces (PSR-3) and the Basic and Extended Coding Standards (PSR-1 and PSR-12).
+
+## Installation
+
+```sh
+composer require open-feature/flagsmith-provider
+```
+
+## Usage
+
+The `FlagsmithProvider` constructor takes a configured Flagsmith client as its only argument:
+
+```php
+$flagsmith = new Flagsmith\Flagsmith('YOUR_FLAGSMITH_API_KEY');
+
+OpenFeatureAPI::setProvider(new FlagsmithProvider($flagsmith));
+```
+
+## Development
+
+### PHP Versioning
+
+This library targets PHP version and newer. As long as you have any compatible version of PHP on your system you should be able to utilize the OpenFeature SDK.
+
+This package also has a `.tool-versions` file for use with PHP version managers like `asdf`.
+
+### Installation and Dependencies
+
+Install dependencies with `composer install`.
+
+We value having as few runtime dependencies as possible. The addition of any dependencies requires careful consideration and review.
+
+### Testing
+
+Run tests with `composer run test`.
---
+++ providers/Flagsmith/src/FlagsmithProvider.php
@@ -0,0 +1,116 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenFeature\Providers\Flagsmith;
+
+use Flagsmith\Exceptions\FlagsmithThrowable;
+use Flagsmith\Flagsmith;
+use Flagsmith\Models\Flags;
+use OpenFeature\implementation\provider\AbstractProvider;
+use OpenFeature\implementation\provider\ResolutionDetailsBuilder;
+use OpenFeature\implementation\provider\ResolutionDetailsFactory;
+use OpenFeature\implementation\provider\ResolutionError;
+use OpenFeature\interfaces\flags\EvaluationContext;
+use OpenFeature\interfaces\provider\ErrorCode;
+use OpenFeature\interfaces\provider\Provider;
+use OpenFeature\interfaces\provider\ResolutionDetails;
+
+use function is_null;
+
+class FlagsmithProvider extends AbstractProvider implements Provider
+{
+    protected static string $NAME = 'FlagsmithProvider';
+
+    public function __construct(
+        private Flagsmith $flagsmith,
+    ) {
+    }
+
+    public function resolveBooleanValue(string $flagKey, bool $defaultValue, ?EvaluationContext $context = null): ResolutionDetails
+    {
+        try {
+            return ResolutionDetailsFactory::fromSuccess(
+                $this->contextualFlagStore($context)->getFlag($flagKey)->getEnabled(),
+            );
+        } catch (FlagsmithThrowable $throwable) {
+            return (new ResolutionDetailsBuilder())
+                ->withValue($defaultValue)
+                ->withError(
+                    new ResolutionError(ErrorCode::GENERAL(), $throwable->getMessage()),
+                )->build();
+        }
+    }
+
+    public function resolveStringValue(string $flagKey, string $defaultValue, ?EvaluationContext $context = null): ResolutionDetails
+    {
+        $resolutionDetails = (new ResolutionDetailsBuilder())->build();
+
+        try {
+            $resolutionDetails->setValue(
+                $this->contextualFlagStore($context)->getFlag($flagKey)->getValue(),
+            );
+        } catch (FlagsmithThrowable) {
+            $resolutionDetails->setValue($defaultValue);
+        }
+
+        return $resolutionDetails;
+    }
+
+    public function resolveIntegerValue(string $flagKey, int $defaultValue, ?EvaluationContext $context = null): ResolutionDetails
+    {
+        $resolutionDetails = (new ResolutionDetailsBuilder())->build();
+
+        try {
+            $resolutionDetails->setValue(
+                $this->contextualFlagStore($context)->getFlag($flagKey)->getValue(),
+            );
+        } catch (FlagsmithThrowable) {
+            $resolutionDetails->setValue($defaultValue);
+        }
+
+        return $resolutionDetails;
+    }
+
+    public function resolveFloatValue(string $flagKey, float $defaultValue, ?EvaluationContext $context = null): ResolutionDetails
+    {
+        $resolutionDetails = (new ResolutionDetailsBuilder())->build();
+
+        try {
+            $resolutionDetails->setValue(
+                $this->contextualFlagStore($context)->getFlag($flagKey)->getValue(),
+            );
+        } catch (FlagsmithThrowable) {
+            $resolutionDetails->setValue($defaultValue);
+        }
+
+        return $resolutionDetails;
+    }
+
+    public function resolveObjectValue(string $flagKey, mixed $defaultValue, ?EvaluationContext $context = null): ResolutionDetails
+    {
+        $builder = new ResolutionDetailsBuilder();
+
+        try {
+            $builder->withValue(
+                $this->contextualFlagStore($context)->getFlag($flagKey)->getValue(),
+            );
+        } catch (FlagsmithThrowable $throwable) {
+            $builder->withValue($defaultValue);
+            $builder->withError(
+                new ResolutionError(ErrorCode::GENERAL(), $throwable->getMessage()),
+            );
+        }
+
+        return $builder->build();
+    }
+
+    private function contextualFlagStore(?EvaluationContext $context = null): Flags
+    {
+        if ($context && !is_null($identifier = $context->getTargetingKey())) {
+            return $this->flagsmith->getIdentityFlags($identifier, (object) $context->getAttributes()->toArray());
+        }
+
+        return $this->flagsmith->getEnvironmentFlags();
+    }
+}
