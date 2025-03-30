be6f4e254a915802750ec22498ec2599864bd0e9
Chris Lightfoot-Wild <github-clw@wild.me.uk>
2025-03-30 20:22:42 +0100

    (chore) remove unused Flagsmith dependencies
    Signed-off-by: Chris Lightfoot-Wild <github-clw@wild.me.uk>

--- providers/Flagsmith/tests/Unit/FlagsmithProviderTest.php
+++ providers/Flagsmith/tests/Unit/FlagsmithProviderTest.php
@@ -5,30 +5,15 @@ declare(strict_types=1);
 namespace OpenFeature\Providers\Flagsmith\Test\Unit;
 
 use Flagsmith\Flagsmith;
-use OpenFeature\Providers\Flagsmith\Config\ConfigFactory;
-use OpenFeature\Providers\Flagsmith\Config\HttpConfig;
 use OpenFeature\Providers\Flagsmith\FlagsmithProvider;
 use OpenFeature\Providers\Flagsmith\Test\TestCase;
 use OpenFeature\interfaces\provider\Provider;
-use Psr\Http\Client\ClientInterface;
-use Psr\Http\Message\RequestFactoryInterface;
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamFactoryInterface;
-use Psr\Http\Message\StreamInterface;
 
 class FlagsmithProviderTest extends TestCase
 {
     public function testCanBeInstantiated(): void
     {
         // Given
-        $config = [
-            'httpConfig' => [
-                'client' => $this->mockery(ClientInterface::class),
-                'requestFactory' => $this->mockery(RequestFactoryInterface::class),
-                'streamFactory' => $this->mockery(StreamFactoryInterface::class),
-            ],
-        ];
         $flagsmith = new Flagsmith('dummy-key');
 
         // When
