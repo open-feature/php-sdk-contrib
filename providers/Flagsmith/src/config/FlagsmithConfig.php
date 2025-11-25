0e66f74b50c652c728466e27fee77ecd5def0c01
wadii <wadii.zaim@flagsmith.com>
2025-11-25 10:17:35 +0100

    feat: implementation-completed

--- providers/Flagsmith/src/config/FlagsmithConfig.php
+++ providers/Flagsmith/src/config/FlagsmithConfig.php
@@ -9,16 +9,41 @@ use InvalidArgumentException;
 class FlagsmithConfig
 {
     private string $apiKey;
+    private ?string $apiUrl;
+    private ?object $customHeaders;
+    private ?int $requestTimeout;
 
-    public function __construct(string $apiKey)
-    {
+    public function __construct(
+        string $apiKey,
+        ?string $apiUrl = null,
+        ?object $customHeaders = null,
+        ?int $requestTimeout = null
+    ) {
         $this->validateApiKey($apiKey);
         $this->apiKey = $apiKey;
+        $this->apiUrl = $apiUrl;
+        $this->customHeaders = $customHeaders;
+        $this->requestTimeout = $requestTimeout;
     }
 
     public function getApiKey(): string
     {
         return $this->apiKey;
+    }
+
+    public function getApiUrl(): ?string
+    {
+        return $this->apiUrl;
+    }
+
+    public function getCustomHeaders(): ?object
+    {
+        return $this->customHeaders;
+    }
+
+    public function getRequestTimeout(): ?int
+    {
+        return $this->requestTimeout;
     }
 
     private function validateApiKey(string $apiKey): void
